449412497a1e98d63af64ed7d12ebff5e0925ac3
Jason Ulloa <jasonulloa@hotmail.com>
2019-05-20 23:23:53 -0700

    Starting Opt2 and adding freeglut to project.

---
+++ CodingPractice/opt2/Opt2.cpp
@@ -0,0 +1,19 @@
+#include <Windows.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <GL/glut.h>
+#include <GL/freeglut.h>
+#include "Opt2.h"
+
+Opt2::Opt2() {
+
+}
+
+Opt2::~Opt2() {
+
+}
+
+void Opt2::run() {
+
+}
---
+++ CodingPractice/opt2/Opt2.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef OPT2_H
+#define OPT2_H
+
+class Opt2 {
+	public:
+		Opt2();
+		~Opt2();
+		void run();
+};
+#endif
